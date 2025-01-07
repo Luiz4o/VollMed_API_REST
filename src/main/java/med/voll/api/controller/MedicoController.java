0f00406f934dbb8b129bcfa19188e180da003466
luizhenrique <henriqueluiz1103@gmail.com>
2025-01-06 22:51:33 -0300

    Requisicao GET para listagem de cadastros medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,17 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -19,9 +22,16 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
-    @Transactional //Por ser um método de escrita tenho que ter a anotação de transactional para o banco de dados rodar
+    @Transactional //Por ser um método de escrita(mas caso fosse update ou delete eu teria que adicionar o transactional
+    // tenho que ter a anotação de transactional para o banco de dados rodar
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        //Pageable é uma classe propria do springboot para trabalhar com paginação dos dados da sua API, e quando passamos
+        // esta anotação serve para modificar parãmetros padrão da classe pageable
+        return repository.findAll(paginacao).map(DadosListagemMedico :: new);
+    }
 }
