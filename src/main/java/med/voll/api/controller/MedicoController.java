fbb189adec4d06c379d78832967a826063cf9e8e
luizhenrique <henriqueluiz1103@gmail.com>
2025-01-08 21:59:29 -0300

    Requisição PUT para atualização do cadastro de médicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -33,5 +30,15 @@ public class MedicoController {
         //Pageable é uma classe propria do springboot para trabalhar com paginação dos dados da sua API, e quando passamos
         // esta anotação serve para modificar parãmetros padrão da classe pageable
         return repository.findAll(paginacao).map(DadosListagemMedico :: new);
+    }
+
+    @PutMapping
+    @Transactional
+    // Este método atualiza as informações do médico com base nos dados recebidos via requisição PUT.
+// A JPA gerencia automaticamente a atualização no banco de dados ao final da transação, sem necessidade de comandos adicionais.
+    // o getReferenceByID como se fosse um ponteiro, então quando atualizamos o objeto a JPA faz o trabalho de atualizar o objeto diretamente no banco.
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
     }
 }
