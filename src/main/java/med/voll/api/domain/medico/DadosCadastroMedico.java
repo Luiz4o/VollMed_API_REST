9f96b984abd682990830305520be2a80ee567c40
luizhenrique <henriqueluiz1103@gmail.com>
2025-01-23 21:37:53 -0300

    Add tratamento do erro EntityNotFoundExcepetion

--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -1,11 +1,11 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
         //Not blank serve para validar que o dado nao pode ser nulo nem vazio e so pode ser usado para strings
